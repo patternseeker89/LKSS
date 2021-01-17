1a416f6d2609a901bcb1e1d4713e7ca7218b9c8a
porfirovskiy <porfirovskiy@gmail.com>
2021-01-17 23:32:52 +0200

    find and add node work fine, and this is awesome!!!

--- StorageTree.php
+++ StorageTree.php
@@ -16,22 +16,19 @@ class StorageTree
         
         $newNode = new Node($key, $data);
         
-        /*if (\is_null($parentNodeKey)) {
-            $this->root = $newNode;
-        } else if(\is_null($this->root->getChildren())) {
-            $this->root->addChild($newNode);          
-        } else {
-            //$parentNode = $this->getNodeByKey11($parentNodeKey);
-            var_dump($parentNode);
-            //$parentNode->addChild($parentNode);
-        }*/
-        
         if (\is_null($parentNodeKey)) {
+            $this->root = $newNode;        
+        } else {
+            $parentNode = $this->getNodeByKey11($parentNodeKey);
+            $parentNode->addChild($newNode);
+        }
+        
+        /*if (\is_null($parentNodeKey)) {
             $this->root = $newNode;      
         } else {
             $this->root->addChild($newNode);
             $this->root->getChildren()[0]->addChild(new Node(13, '=========='));
-        }
+        }*/
         
         $this->saveNodeKey($newNode->getKey());
     }
@@ -43,8 +40,12 @@ class StorageTree
     
     public function getNodeByKey11(int $key): ?Node 
     {
-        //return new Node('data');     
-        return $this->root->getNodeByKey($key, $this->root);
+        if ($this->root->getKey() === $key) {    
+            return $this->root;
+        } else {
+            return $this->root->getNodeByKey($key, $this->root);
+        }
+        //return $this->root->getNodeByKey($key, $this->root);
     }
     
     public function isEmpty(): bool
--- index.php
+++ index.php
@@ -4,12 +4,30 @@ require_once 'Node.php';
 require_once 'StorageTree.php';
 
 $storage = new StorageTree();
+
 $storage->insertNode(1, null,'test data');
 
 $storage->insertNode(2, 1, 'test data 2');
 
 $storage->insertNode(3, 1, 'test data 3');
 
+$storage->insertNode(4, 3, 'test data 4');
 
-//var_dump($storage);
-var_dump($storage->getNodeByKey11(13));
+$storage->insertNode(5, 3, 'test data 5');
+
+$storage->insertNode(6, 4, 'test data 6');
+
+$storage->insertNode(7, 4, 'test data 7');
+
+$storage->insertNode(8, 6, 'test data 7');
+
+$storage->insertNode(9, 6, 'test data 7');
+
+$storage->insertNode(10, 9, 'test data 7');
+
+$storage->insertNode(11, 2, 'test data 7');
+
+$storage->insertNode(12, 2, 'test data 7');
+
+var_dump($storage);
+//var_dump($storage->getNodeByKey11(4));
